7bfaa266b54820c7f9c061ee8be1c8dffcf7dfc6
Aniket Mulmule <Aniket.Mulmule@extentia05.local>
2017-11-06 15:04:59 +0530

    modified webform1

--- Web_Project/Web_Project/WebForm1.aspx.designer.cs
+++ Web_Project/Web_Project/WebForm1.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace Web_Project {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// Button2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
+        
+        /// <summary>
         /// Button1 control.
         /// </summary>
         /// <remarks>
